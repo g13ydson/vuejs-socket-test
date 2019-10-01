3e0118d82d1d6efb1db1d14f92e2d0ae69f98d4e
Gleydson <tavares.gleydson@gmail.com>
2019-10-01 10:53:57 -0300

    adjust socket connection

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <div id="app">
+      <v-btn @click="disconnect" v-if="status === 'connected'" color="error">Disconnect</v-btn>
+      <v-btn @click="connect" v-if="status === 'disconnected'" color="primary">Connect</v-btn>
+
+      <v-card class="mx-auto" max-width="800" tile>
+        <div v-if="status === 'connected'">
+          <v-list-item v-for="(message, i) in messages" :key="i">
+            <v-list-item-content>
+              <v-list-item-title v-text="message.sended_at + ' : '+ message.body" />
+            </v-list-item-content>
+          </v-list-item>
+        </div>
+      </v-card>
+    </div>
+  </v-container>
 </template>
 
 <script>
+import { Socket } from "phoenix";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  methods: {
+    connect() {
+      let socket = new Socket("ws://localhost:4000/socket");
+      socket.connect();
+      // Join in the links channel
+      this.channel = socket.channel("room:1", {});
+      this.channel
+        .join()
+        .receive("ok", resp => {
+          this.status = "connected";
+          console.log("Joined successfully", resp);
+        })
+        .receive("error", resp => {
+          console.log("Unable to join", resp);
+        });
+
+      this.channel.on("messages", payload => {
+        this.messages = payload.messages;
+      });
+      this.channel.on("new_message", payload => {
+        console.log(payload);
+        this.messages = this.messages.concat(payload.message);
+      });
+    },
+    disconnect() {
+      this.channel.leave();
+      this.status = "disconnected";
+    }
+  },
+  data() {
+    return {
+      message: "",
+      messages: [],
+      status: "disconnected"
+    };
+  }
+};
+</script>
